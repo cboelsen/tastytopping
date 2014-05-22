5c7b19c4eeb1c16c875730ca692d1e57ff7f4752
Christian Boelsen <christian.boelsen@hds.com>
2014-05-22 16:29:36 +0100

    Test against the latest version of django.

--- tests/tests.py
+++ tests/tests.py
@@ -483,6 +483,7 @@ class IntegrationTests(TestsBase):
 
 
     # FEATURES:
+    # TODO Don't raise ResourceDeleted when unable to connect to API.
     # TODO Don't update field if value the same.
     # TODO Only GET a new Resource when fields needed?!?! Should the exceptions be delayed?!? Will it be confusing?!?
     # TODO Allow files to be passed when tastypie supports it (https://github.com/cboelsen/tastytopping/issues/1)
